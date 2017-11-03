8526c37f3807438cbb6f80196a728b3a7d41f466
Ivan Velichko <iximiuz@gmail.com>
2017-11-03 22:34:12 +0100

    v0.2.3 Support more WMIC output formats

--- tests/test-win32.js
+++ tests/test-win32.js
@@ -10,6 +10,30 @@ var output = 'FreeSpace     Name  Size\r\n' +
              '16610680832   F:    120101797888\r\n' +
              '16610680832   G:    120101797888\r\n\r\n';
 
+var output2 = 'FreeSpace    Name  Size          \r\n' +
+              '70402592768  C:    119507251200  \r\n' +
+              '             D:                  \r\n\r\n';
+
+var output3 = 'FreeSpace     Name  Size          \r\n' +
+              '464075616256  C:    998659936256  \r\n' +
+              '0             D:    606087168     \r\n' +
+              '0             E:    1453686784    \r\n' +
+              '              F:                  \r\n' +
+              '0             G:    2749777920    \r\n' +
+              '0             H:    3676667904    \r\n' +
+              '0             I:    739725312     \r\n\r\n';
+
+var output4 = 'FreeSpace     Name  Size          \r\n' +
+              '455402688512  C:    998659936256  \r\n' +
+              '0             D:    606087168     \r\n' +
+              '0             E:    1453686784    \r\n' +
+              '              F:                  \r\n' +
+              '0             G:    2749777920    \r\n' +
+              '0             H:    3676667904    \r\n' +
+              '0             I:    739725312     \r\n' +
+              '455402688512  S:    998659936256  \r\n\r\n';
+
+
 test('(Win32) it parses (I)', function(assert) {
     var parsed = diskusage.parse(output, 'C:');
     assert.equals(parsed.total, 136256155648);
@@ -23,6 +47,41 @@ test('(Win32) it parses (II)', function(assert) {
     assert.equals(parsed.total, 59496448);
     assert.equals(parsed.used, 59496448);
     assert.equals(parsed.available, 0);
+    assert.end();
+});
+
+test('(Win32) it parses (III)', function(assert) {
+    var parsed = diskusage.parse(output2, 'C:');
+    assert.equals(parsed.total, 119507251200);
+    assert.equals(parsed.used, 119507251200 - 70402592768);
+    assert.equals(parsed.available, 70402592768);
+
+    parsed = diskusage.parse(output2, 'D:');
+    assert.equals(parsed.total, 0);
+    assert.equals(parsed.used, 0);
+    assert.equals(parsed.available, 0);
+    assert.end();
+});
+
+test('(Win32) it parses (IV)', function(assert) {
+    var parsed = diskusage.parse(output3, 'F:');
+    assert.equals(parsed.total, 0);
+    assert.equals(parsed.used, 0);
+    assert.equals(parsed.available, 0);
+    assert.end();
+});
+
+test('(Win32) it parses (V)', function(assert) {
+    var parsed = diskusage.parse(output4, 'F:');
+    assert.equals(parsed.total, 0);
+    assert.equals(parsed.used, 0);
+    assert.equals(parsed.available, 0);
+
+    parsed = diskusage.parse(output4, 'S:');
+    assert.equals(parsed.total, 998659936256);
+    assert.equals(parsed.used, 998659936256 - 455402688512);
+    assert.equals(parsed.available, 455402688512);
+
     assert.end();
 });
 
